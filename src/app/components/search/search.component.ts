bbfdc4bac5c981002681b4a2bba1251c5c930c4e
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-09-24 21:30:07 +0200

    added new page and changin

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FilmsService } from '../../services/films.service';
 
 @Component({
   selector: 'app-search',
@@ -7,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  public searchString = ''
+
+  constructor(private _filmsService: FilmsService) {
+  }
+
+  public updateValue(event: Event) {
+    const target = event.target as HTMLInputElement
+    this.searchString = target.value
+  }
+
+  public updateSearch(event: Event) {
+    this._filmsService.searchString = this.searchString
+  }
 
 }
